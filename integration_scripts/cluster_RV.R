d09b9cf24a9de52c3bd5feb642b4c0a43d188765
rssealfon <rsealfon@simonsfoundation.org>
2021-05-03 12:59:06 -0400

    Update cluster_RV.R

--- integration_scripts/cluster_RV.R
+++ integration_scripts/cluster_RV.R
@@ -7,8 +7,7 @@ args = commandArgs(trailingOnly = TRUE) #k hyperparameter for k means clustering
 library(MASS)
 library(data.table)
 
-#genes = c('lef1')
-#genes = c('cdh1', 'jag1', 'lef1', 'sall1', 'six2', 'sox9', 'troma1', 'wt1');
+
 genes = c('cdh1', 'cdh6', 'cldn5', 'emx2', 'erbb4', 'foxc2', 'hnf1b', 'jag1', 'lef1', 'lhx1', 'mafb', 'mecom', 'pax2', 'pou3f3', 'sall1', 'six2', 'sox9', 'troma1', 'wt1')
 imgs_cdh1 = c('00000007', '00000009', '00000010', '00000011', '00000012', '00000013', '00000014', '00000060', '00000061', '00000062', '00000063', '00000064', '00000065', '00000066', '00000067', '00000068', '00000069', '00000115', '00000116', '00000117')
 imgs_cdh6 = c('00000115', '00000116', '00000117')
@@ -22,7 +21,6 @@ imgs_lef1 = c('00000001', '00000002', '00000003')
 imgs_lhx1 = c('00000060', '00000061', '00000062', '00000063')
 imgs_mafb = c('00000103', '00000104', '00000105', '00000106')
 imgs_mecom = c('00000129', '00000130', '00000131')
-#imgs_pappa2 = c('00000122', '00000123', '00000124', '00000125')
 imgs_pax2 = c('00000060', '00000061', '00000062', '00000063')
 imgs_pou3f3 = c('00000103', '00000104', '00000105', '00000106')
 imgs_sall1 = c('00000001', '00000002', '00000003')
@@ -73,18 +71,13 @@ for (i in c(1:length(genes))){
     assign(paste(genes[i], "_v", sep=""), rep(0, length(as.vector(as.matrix(get(paste(genes[1], "_",  imgs_cdh1[1], sep="")))))))       
     gene_v = get(paste(genes[i], "_v", sep=""))
     for (j in 1:length(gene_imgs)){
-        #print(as.vector(scale(as.vector(as.matrix(get(paste(genes[i], "_",    gene_imgs[j], sep=""))))))[50000])
         gene_v = gene_v + as.vector(scale(as.vector(as.matrix(get(paste(genes[i], "_",  gene_imgs[j], sep=""))))))      
-        #print(gene_v[50000])
     }
     gene_v = gene_v / length(gene_imgs) 
     assign(paste(genes[i], "_v", sep=""), gene_v)
-    #for (k in 50000:50010) {
-    #   print(paste(genes[i], ":", gene_v[k], sep=""))
-    # }
+  
 }
 
-#write.table(matrix(get("lef1_v"), nrow = 62634, ncol = 190), "lef1_avg.txt", row.names=FALSE, col.names=FALSE)
 
 all <- cbind(get("cdh1_v"), get("cdh6_v"), get("cldn5_v"), get("emx2_v"), get("erbb4_v"), get("foxc2_v"), get("hnf1b_v"), get("jag1_v"), get("lef1_v"), get("lhx1_v"), get("mafb_v"), get("mecom_v"), get("pax2_v"), get("pou3f3_v"), get("sall1_v"), get("six2_v"), get("sox9_v"), get("troma1_v"), get("wt1_v"))
 all_s <- scale(all)
